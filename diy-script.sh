67f0c7027530f7c845e647d6604679515a3dbf5c
stardust-1 <36785148+stardust-1@users.noreply.github.com>
2024-12-14 12:42:57 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -37,6 +37,12 @@ git clone --depth=1 https://github.com/kenzok8/openwrt-packages package/kenzo
 # 更换golong 1.22
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
+# Themes
+git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
+git_sparse_clone main https://github.com/haiibo/packages luci-theme-atmaterial luci-theme-opentomcat luci-theme-netgear
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
