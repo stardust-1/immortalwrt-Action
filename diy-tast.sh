c2348f79841195a9177f48c4885117dd7e959011
stardust-1 <36785148+stardust-1@users.noreply.github.com>
2024-12-14 18:31:52 +0800

    Update diy-tast.sh

--- diy-tast.sh
+++ diy-tast.sh
@@ -47,5 +47,12 @@ git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
 git_sparse_clone main https://github.com/linkease/istore luci
 
+# Themes
+git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
+git_sparse_clone main https://github.com/haiibo/packages luci-theme-atmaterial luci-theme-opentomcat luci-theme-netgear
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
